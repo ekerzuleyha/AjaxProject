af4c19de16f97734cec8753c4e3920ff3649a8b1
zuleyhaeker.16 <zuleyhaeker.16@gmail.com>
2024-06-27 19:25:57 +0300

    Add project files.

---
+++ AjaxProject/DAL/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProject.DAL
+{
+    //DbContext den miras aldık
+   
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=DESKTOP-58O17EO;initial Catalog=AjaxProjectDb;integrated Security=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+        //dbsetin türü product alacağı property ismi de products olcak.
+    }
+}
